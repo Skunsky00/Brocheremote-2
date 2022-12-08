6a966863493ed605ffda13dbf8d20f24f72f9250
Jacob Johnson <travelbroche@gmail.com>
2022-12-08 14:43:54 -0500

    Initial Commit

---
+++ Broche/BrocheApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrocheApp.swift
+//  Broche
+//
+//  Created by Jacob Johnson on 12/8/22.
+//
+
+import SwiftUI
+
+@main
+struct BrocheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
